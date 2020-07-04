21414fd7191019d7ae1195ed966d5b0481e61617
ARYA <aryaRbarua@gmail.com>
2020-07-04 20:52:30 +0530

    database addition done

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require("express");
-// const fs = require("fs");
 const path = require("path");
 const app = express();
+const mongoose = require("mongoose");mongoose.connect('mongodb://localhost/contactDance', {useNewUrlParser: true});
+const bodyparser = require('body-parser');
 const port = 8000;
+
+
+//define mongoose schema
+
+var contactschema = new mongoose.Schema({
+    fname: String,
+    lname: String,
+    email: String,
+    contactno: String,
+    
+    
+  });
+  const contacts = mongoose.model('contacts', contactschema);
 
 
 //express stuffs
@@ -23,6 +37,17 @@ app.get('/contact.pug', (req, res)=>{
     const params = {}
     res.status(200).render('contact.pug', params);
 })
+app.post('/contact.pug', (req, res)=>{
+    
+    var myData = new contacts(req.body);
+    myData.save().then(()=>{
+        res.send("this is saved in database")
+    })
+    .catch(()=>{
+        res.status(404).send("this is not saved in database")
+    })
+    // res.status(200).render('contact.pug');
+});
 
 // START THE SERVER
 app.listen(port, ()=>{
